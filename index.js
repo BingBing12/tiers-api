2a0fc1ae34f8c7b8bbb4b3d36f9562344063c7a4
Robin Tainton <robintainton0509@gmail.com>
2022-02-13 19:35:07 +0200

    implemented post requests for new user and influencer

--- index.js
+++ index.js
@@ -38,29 +38,68 @@ app.get("/", function (req, res) {
   res.send("connected");
 });
 
-app.get("/influencers", function (req, res) {
-  Influencer.find(function (err, influencers) {
-    if (!err && req.headers.api_key === API_KEY) {
-      res.send(influencers);
-    } else if (req.headers.api_key !== API_KEY) {
-      res.send("unauthorized access");
+app
+  .route("/influencers")
+  .get((req, res) => {
+    Influencer.find((err, influencers) => {
+      if (!err && req.headers.api_key === API_KEY) {
+        res.send(influencers);
+      } else if (req.headers.api_key !== API_KEY) {
+        res.send("unauthorized access");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newInfluencer = new Influencer({
+      name: req.body.name,
+      age: req.body.age,
+    });
+    if (req.headers.api_key === API_KEY) {
+      newInfluencer.save((err) => {
+        if (!err) {
+          res.send("successful");
+        } else {
+          res.send("unsuccessful");
+        }
+      });
     } else {
-      res.send(err);
+      res.send("unauthorized access");
     }
   });
-});
 
-app.get("/users", function (req, res) {
-  User.find(function (err, users) {
-    if (!err && req.headers.api_key === API_KEY) {
-      res.send(users);
+app
+  .route("/users")
+  .get((req, res) => {
+    User.find((err, users) => {
+      if (!err && req.headers.api_key === API_KEY) {
+        res.send(users);
+      } else if (req.headers.api_key !== API_KEY) {
+        res.send("unauthorized access");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post((req, res) => {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    if (req.headers.api_key === API_KEY) {
+      newUser.save((err) => {
+        if (!err) {
+          res.send("successful");
+        } else {
+          res.send("unsuccessful");
+        }
+      });
     } else if (req.headers.api_key !== API_KEY) {
       res.send("unauthorized access");
-    } else {
-      res.send(err);
     }
   });
-});
 
 app.listen(3000, function () {
   console.log("connected to port 3000");
