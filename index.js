6501e28a650a761172bd46e6f2d1ef50ba7cf9db
Robin Tainton <robintainton0509@gmail.com>
2022-02-15 12:15:37 +0200

    login and register routes and authentication functionality with password hashing

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const { type } = require("express/lib/response");
 const app = express();
 const dotenv = require("dotenv").config();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const API_KEY = process.env.api_key;
 
@@ -16,9 +19,11 @@ app.use(
 );
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/ratingDB", {
-  useNewUrlParser: true,
-});
+mongoose
+  .connect("mongodb://localhost:27017/ratingDB", {
+    useNewUrlParser: true,
+  })
+  .then(console.log("connected to db"));
 
 const influencerSchema = {
   name: String,
@@ -69,37 +74,28 @@ app
     }
   });
 
-app
-  .route("/users")
-  .get((req, res) => {
-    User.find((err, users) => {
-      if (!err && req.headers.api_key === API_KEY) {
-        res.send(users);
+app.route("/register").post((req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    if (!err) {
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hash,
+      });
+      if (req.headers.api_key === API_KEY) {
+        newUser.save((err) => {
+          if (!err) {
+            res.send("successful");
+          } else {
+            res.send("unsuccessful");
+          }
+        });
       } else if (req.headers.api_key !== API_KEY) {
         res.send("unauthorized access");
-      } else {
-        res.send(err);
       }
-    });
-  })
-  .post((req, res) => {
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-    if (req.headers.api_key === API_KEY) {
-      newUser.save((err) => {
-        if (!err) {
-          res.send("successful");
-        } else {
-          res.send("unsuccessful");
-        }
-      });
-    } else if (req.headers.api_key !== API_KEY) {
-      res.send("unauthorized access");
     }
   });
+});
 
 app.route("/influencers/:influencerID").get((req, res) => {
   Influencer.findOne({ _id: req.params.influencerID }, (err, result) => {
@@ -113,10 +109,27 @@ app.route("/influencers/:influencerID").get((req, res) => {
   });
 });
 
+app.route("/login/:userEmail").get((req, res) => {
+  User.findOne({ email: req.params.userEmail }, (err, entry) => {
+    if (!err && req.headers.api_key === API_KEY) {
+      bcrypt.compare(req.body.password, entry.password, function (err, result) {
+        if (!err) {
+          res.send(result);
+        }
+      });
+    } else if (req.headers.api_key !== API_KEY) {
+      res.send("unauthorized");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.route("/users/:userID").get((req, res) => {
   User.findOne({ _id: req.params.userID }, (err, result) => {
     if (!err && req.headers.api_key === API_KEY) {
-      res.send(result);
+      const { password, ...others } = result._doc;
+      res.send(others);
     } else if (req.headers.api_key !== API_KEY) {
       res.send("unauthorized");
     } else {
